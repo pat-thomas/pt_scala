dbc4b709e98bd770129aaf4918cddbd94895e61a
Pat Thomas <pthomas@Relay-036-MBPr.local>
2016-04-08 17:58:08 -0400

    ping pong akka example

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,51 @@
 import com.typesafe.akka.extension.quartz.QuartzSchedulerExtension
 import akka.actor._
 
-class HelloActor(myName: String) extends Actor {
+case object PingMessage
+case object PongMessage
+case object StartMessage
+case object StopMessage
+
+class Ping(pong: ActorRef) extends Actor {
+  var count = 0
+  def incrementAndPrint = {
+    count += 1
+    println("ping")
+  }
   def receive = {
-    case "hello" => println("hello from %s".format(myName))
-    case _ => println("'huh?', said %s".format(myName))
+    case StartMessage =>
+      incrementAndPrint
+      pong ! PingMessage
+    case PongMessage =>
+      incrementAndPrint
+      if (count > 99) {
+        sender ! StopMessage
+        println("ping stopped")
+        context.stop(self)
+      } else {
+        sender ! PingMessage
+      }
+  }
+}
+
+class Pong extends Actor {
+  def receive = {
+    case PingMessage =>
+      println("  pong")
+      sender ! PongMessage
+    case StopMessage =>
+      println("pong stopped")
+      context.stop(self)
   }
 }
 
 object Main extends App {
-  val system = ActorSystem("HelloSystem")
-  val helloActor = system.actorOf(Props(new HelloActor("Fred")), name = "helloactor")
-  helloActor ! "hello"
-  helloActor ! "buenos dias"
+  val system = ActorSystem("PingPongSystem")
+  val pong = system.actorOf(Props[Pong], name = "pong")
+  val ping = system.actorOf(Props(new Ping(pong)), name = "ping")
 
-  val scheduler = QuartzSchedulerExtension(system)
+  ping ! StartMessage
+
+  //val scheduler = QuartzSchedulerExtension(system)
 }
 
