4c4f625acc54b7676de9bc8dc04c517038064926
Pat Thomas <pthomas@Relay-036-MBPr.local>
2016-04-19 10:05:20 -0400

    clean stuff up more

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,51 +1,13 @@
 import com.typesafe.akka.extension.quartz.QuartzSchedulerExtension
 import akka.actor._
-
-case object PingMessage
-case object PongMessage
-case object StartMessage
-case object StopMessage
-
-class Ping(pong: ActorRef) extends Actor {
-  var count = 0
-  def incrementAndPrint = {
-    count += 1
-    println("ping")
-  }
-  def receive = {
-    case StartMessage =>
-      incrementAndPrint
-      pong ! PingMessage
-    case PongMessage =>
-      incrementAndPrint
-      if (count > 99) {
-        sender ! StopMessage
-        println("ping stopped")
-        context.stop(self)
-      } else {
-        sender ! PingMessage
-      }
-  }
-}
-
-class Pong extends Actor {
-  def receive = {
-    case PingMessage =>
-      println("  pong")
-      sender ! PongMessage
-    case StopMessage =>
-      println("pong stopped")
-      context.stop(self)
-  }
-}
+import java.io.File._
 
 object Main extends App {
-  val system = ActorSystem("PingPongSystem")
-  val pong = system.actorOf(Props[Pong], name = "pong")
-  val ping = system.actorOf(Props(new Ping(pong)), name = "ping")
-
-  ping ! StartMessage
-
-  //val scheduler = QuartzSchedulerExtension(system)
+  var counter = 0
+  while(true) {
+    println(counter)
+    counter += 1
+    Thread.sleep(1000)
+  }
 }
 
