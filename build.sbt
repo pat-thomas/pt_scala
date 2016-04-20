c75ae70b4f0bf65470395035e3aeeb82cae56343
Pat Thomas <pthomas@Relay-036-MBPr.local>
2016-04-20 17:29:57 -0400

    http client stuff

--- build.sbt
+++ build.sbt
@@ -6,6 +6,7 @@ scalaVersion := "2.11.8"
 
 libraryDependencies += "com.enragedginger" %% "akka-quartz-scheduler" % "1.5.0-akka-2.4.x"
 libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.4.1"
+libraryDependencies += "com.typesafe.play" %% "play-ws" % "2.4.3"
 
 mainClass in (Compile, run) := Some("pt.Main")
     
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,27 @@
 import com.typesafe.akka.extension.quartz.QuartzSchedulerExtension
 import akka.actor._
-import java.io.File._
+import java.io._
+import play.api.libs.ws.ning.NingWSClient
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main extends App {
-  var counter = 0
-  while(true) {
-    println(counter)
-    counter += 1
-    Thread.sleep(1000)
+  val wsClient = NingWSClient()
+
+  def writeToFile(pw: PrintWriter, text: String) = {
+    pw.write(text)
   }
+
+  def simpleGet(url: String) = {
+    wsClient
+      .url(url)
+      .get()
+      .map { wsResponse =>
+        println(wsResponse.body)
+      }
+  }
+
+  simpleGet("http://utphilly.com")
+  simpleGet("http://johnnybrendas.com")
+  simpleGet("http://bootandsaddlephilly.com")
 }
 
